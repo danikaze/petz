b43c67378dfac9bced233aba2d6de935627b04af
danikaze <danikaze@gmail.com>
2022-09-29 01:20:33 +0900

    use-callback-ref

---
+++ src/utils/use-callback-ref.ts
@@ -0,0 +1,31 @@
+import { MutableRefObject, useState } from 'react';
+
+export function useCallbackRef<T>(
+  initialValue: T | null,
+  callback: (newValue: T | null, lastValue: T | null) => void
+): MutableRefObject<T | null> {
+  const [ref] = useState(() => ({
+    // value
+    value: initialValue,
+    // last callback
+    callback,
+    // "memoized" public interface
+    facade: {
+      get current() {
+        return ref.value;
+      },
+      set current(value) {
+        const last = ref.value;
+
+        if (last !== value) {
+          ref.value = value;
+          ref.callback(value, last);
+        }
+      },
+    },
+  }));
+  // update callback
+  ref.callback = callback;
+
+  return ref.facade;
+}
