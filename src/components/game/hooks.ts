8e2c09c12a779ce1b916fc44487da49d251b850b
danikaze <danikaze@gmail.com>
2022-09-29 09:41:54 +0900

    Handle basic Phaser resize logic

--- src/components/game/hooks.ts
+++ src/components/game/hooks.ts
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 import { MainGame } from '@game';
 import { getLogger } from '@src/utils/logger';
 import { useCallbackRef } from '@src/utils/use-callback-ref';
 
 const logger = getLogger('Game');
 
+interface State {
+  html: HTMLHtmlElement;
+  canvas: HTMLCanvasElement;
+  mainGame: MainGame;
+}
+
 export function useGame() {
+  const [state, setState] = useState<State | undefined>();
   const refs = {
     parent: useCallbackRef<HTMLDivElement>(null, initGame),
   };
+
+  useEffect(() => {
+    if (!state) return;
+    window.addEventListener('resize', resizeCanvas);
+    return destroy;
+  }, [state]);
 
   function initGame(parent: HTMLDivElement | null) {
     if (!parent) return;
@@ -15,6 +29,30 @@ export function useGame() {
     const game = new MainGame({ parent });
     game.setup();
     logger.info('Game initialized', game);
+
+    const html = document.querySelector('html');
+    const canvas = parent.querySelector('canvas');
+
+    if (!html || !canvas) {
+      throw new Error('Something went wrong with the canvas');
+    }
+
+    setState({
+      html,
+      canvas,
+      mainGame: game,
+    });
+  }
+
+  function resizeCanvas(): void {
+    if (!state) return;
+    const { mainGame, html } = state;
+
+    mainGame.game.scale.resize(html.clientWidth, html.clientHeight);
+  }
+
+  function destroy() {
+    window.removeEventListener('resize', resizeCanvas);
   }
 
   return {
